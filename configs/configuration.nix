200e3a17855c2b2ca591f2024219a8956fa333ca
Nero <nerostudyhat@gmail.com>
2024-01-27 11:47:17 -0500

    Small fixes.

--- configs/configuration.nix
+++ configs/configuration.nix
@@ -16,14 +16,7 @@
   networking.hostName = "stardom";
   networking.networkmanager.enable = true;
 
-  time.timeZone = "Amaerica/New_York";
-
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "us";
-    useXkbConfig = true; # use xkb.options in tty.
-  };
+  time.timeZone = "America/New_York";
 
   boot.loader = {
     efi = {
