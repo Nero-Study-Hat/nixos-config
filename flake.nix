5c882dbc9858d93d5219a3910d3be22a91c610b2
Nero <nerostudyhat@gmail.com>
2024-02-03 11:17:28 -0500

    small changes to prev commit

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
             nixdom = lib.nixosSystem {
                 system = "x86_64-linux";
                 modules = [ ./configs/configuration.nix ];
-                _module.args = { inherit inputs; };
+                _modules.args = { inherit inputs; };
             };
         };
         homeConfigurations = {
