09add9bd59ca2d57801db00b4a6ba31f61eadd9c
Nero <nerostudyhat@gmail.com>
2024-02-08 13:04:50 -0500

    Updated packages files.

---
+++ home-manager/programs/vscode.nix
@@ -0,0 +1,13 @@
+{
+    programs.vscode = {
+        enable = true;
+        package = pkgs.vscode;
+        enableUpdateCheck = false;
+        enableExtensionUpdateCheck = false;
+
+        extensions = with pkgs.vscode-extensions; [
+            bbenoist.Nix
+            PKief.material-icon-theme
+        ]
+    };
+}
