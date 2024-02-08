11a23d51846e0da12ecdd52a6489f0328636fc22
Nero <nerostudyhat@gmail.com>
2024-02-08 13:18:54 -0500

    Flake change test.

--- flake.nix
+++ flake.nix
@@ -11,19 +11,19 @@
     let
         system = "x86_64-linux";
         nixpkgs.config.allowUnfree = true;
-        lib = nixpkgs.lib;
+        # lib = nixpkgs.lib;
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
         defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
     in {
         nixosConfigurations = {
-            stardom = lib.nixosSystem {
+            stardom = nixpkgs.lib.nixosSystem {
                 inherit system;
                 modules = [ ./configs/configuration.nix ];
                 specialArgs = { inherit inputs; };
             };
         };
         homeConfigurations = {
-            nero = home-manager.lib.homeManagerConfiguration {
+            nero = home-manager.nixpkgs.lib.homeManagerConfiguration {
                 inherit pkgs;
                 modules = [ ./home-manager/home.nix ];
             };
