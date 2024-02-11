6db3747b2b2fa17c75a7aaa01c442b3586e9b56b
nero-obsidian <nerostudyhat@gmail.com>
2024-02-11 13:45:32 -0500

    added a comment

--- home-manager/programs/vscode.nix
+++ home-manager/programs/vscode.nix
@@ -7,6 +7,7 @@
         enableUpdateCheck = false;
         enableExtensionUpdateCheck = false;
 
+        # ext ids must be all lowercase
         extensions = with pkgs.vscode-extensions; [
             bbenoist.nix
             pkief.material-icon-theme
