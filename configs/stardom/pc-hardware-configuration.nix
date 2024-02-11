2f320fd708628c7345e5569bf191e13809d7a145
nero-obsidian <nerostudyhat@gmail.com>
2024-02-11 12:02:52 -0500

    PC hardware conf edit for amdgpu.

--- configs/stardom/pc-hardware-configuration.nix
+++ configs/stardom/pc-hardware-configuration.nix
@@ -5,7 +5,7 @@
 
 	boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
 	boot.initrd.kernelModules = [ "amdgpu" ];
-	# boot.kernelModules = [ "kvm-amd" ];
+	boot.kernelModules = [ "kvm-amd" ];
 	boot.extraModulePackages = [ ];
 
 	fileSystems."/".device = "/dev/disk/by-label/nixos";
@@ -21,7 +21,28 @@
 	nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 	hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-	hardware.opengl.extraPackages = with pkgs; [
-	rocmPackages.clr.icd
+	hardware = {
+	opengl = {
+		enable = true;
+
+		# For AMD vulkan support
+		driSupport = true;
+		driSupport32Bit = true;
+
+		extraPackages = with pkgs; [
+			amdvlk
+			mesa_23
+			rocmPackages.clr.icd
+		];
+
+		# For 32 bit applications 
+		# Only available on unstable
+		extraPackages32 = with pkgs; [
+			driversi686Linux.amdvlk
+		];
+	};
+
+	systemd.tmpfiles.rules = [
+		"L+    /opt/rocm/hip   -    -    -     -    ${pkgs.hip}"
 	];
 }
