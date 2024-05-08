e05ce2b5c26db2dfaa2986057d66c995d163311d
Nero-Study-Hat <nerostudyhat@gmail.com>
2024-05-08 12:02:33 -0400

    Update configuration.nix

--- configs/stardom/configuration.nix
+++ configs/stardom/configuration.nix
@@ -39,8 +39,18 @@
 
 	services.xserver.enable = true;
 	services.xserver.videoDrivers = [ "amdgpu" ];
-	services.xserver.displayManager.sddm.enable = true;
+	services.displayManager.sddm.enable = true;
 	services.xserver.desktopManager.plasma5.enable = true;
+
+	security.rtkit.enable = true;
+	services.pipewire = {
+		enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
+		pulse.enable = true;
+		# If you want to use JACK applications, uncomment this
+		#jack.enable = true;
+	};
 
 	system.stateVersion = "24.05";
 }
