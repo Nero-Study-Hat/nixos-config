911260676cd8a39fc2172ed75689486568258ec8
Nero-Study-Hat <nerostudyhat@gmail.com>
2024-08-11 22:59:38 -0400

    Filled out more of desktop-switcher C program.

--- scripts/hyprland-workspaces/C-desktop-switcher/desktop-switcher.c
+++ scripts/hyprland-workspaces/C-desktop-switcher/desktop-switcher.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 // cmd for testing (dev dir): gcc desktop-switcher.c -o demo && ./demo && rm demo
 
@@ -12,7 +13,7 @@ void getStdoutFromCommand(char* str)
 }
 
 /** 
- * @param   {name} - {description}
+ * @param   target_value - desired activity or desktop direction change
  * @param   {name} - {description}
  * @param   {name} - {description}
 */
@@ -24,30 +25,74 @@ int main(int argc, char* argv[])
 
     char currentDesktop[3];
     getStdoutFromCommand(currentDesktop);
-    int currentDesktopNum = atoi(currentDesktop);
+    const int currentDesktopNum = atoi(currentDesktop);
+    int targetDesktopNum;
+    int modifierNum;
 
-    printf("%i \n", currentDesktopNum);
-    
+    const int limit = rows + 1;
+    char direction[] = "left";
+    for (int i = 1; i < limit; i++) {
+        // left most colum
+        const int x = i * columns - (columns - 1);
+        if (x == currentDesktopNum && 0 == strcmp(argv[1], "left")) {
+            if (wrapEnable == true) {
+                targetDesktopNum = currentDesktopNum + (columns - 1);
+                printf("Left most column wrap: %i \n", targetDesktopNum);
+            }
+            else {
+                return 0;
+            }
+        }
+        // right most column
+        const int y = i * columns;
+        if (x == currentDesktopNum && 0 == strcmp(argv[1], "right")) {
+            if (wrapEnable == true) {
+                targetDesktopNum = currentDesktopNum - (columns - 1);
+                printf("Right most column wrap: %i \n", i);
+            }
+            else {
+                return 0;
+            }
+        }
+    }
+
+    // top row
+    if (currentDesktopNum >= 1 && currentDesktopNum <= columns && 0 == strcmp(argv[1], "up")) {
+        if (wrapEnable == true) {
+            targetDesktopNum = currentDesktopNum + (columns * (rows -1));
+            printf("top most row wrap: %i \n", targetDesktopNum);
+        }
+        else {
+            return 0;
+        }
+    }
+    // bottom row
+    else if (currentDesktopNum >= (columns * (rows - 1) + 1) && currentDesktopNum <= (rows * columns) && 0 == strcmp(argv[1], "down")) {
+        if (wrapEnable == true) {
+            targetDesktopNum = currentDesktopNum - (columns * (rows -1));
+            printf("bottom most column wrap: %i \n", targetDesktopNum);
+        }
+        else {
+            return 0;
+        }
+    }
+
+    if (strcmp(argv[1], "right") == 0) {
+        modifierNum = 1;
+    }
+    else if (strcmp(argv[1], "left") == 0) {
+        modifierNum = -1;
+    }
+    else if (strcmp(argv[1], "up") == 0) {
+        modifierNum = -1;
+    }
+    else if (strcmp(argv[1], "down") == 0) {
+        modifierNum = -1;
+    }
+
+    targetDesktopNum = currentDesktopNum + modifierNum;
+    printf("target no wrap: %i \n", targetDesktopNum);
+
     return 0;
 }
 
-/**
- * Execute a command and get the result.
- *
- * @param   cmd - The system command to run.
- * @return  The string command line output of the command.
- */
-
-
-/*  Gets the correct value and prints to stdout when used in main()
-
-    FILE *cmd=popen("hyprctl printdesk | awk '{print $3;}' | sed 's/.$//'", "r");
-    char result[24]={0x0};
-    while (fgets(result, sizeof(result), cmd) !=NULL)
-           printf("%s\n", result);
-    pclose(cmd);
-
-    // printf("%i", test);
-    return 0;
-
-*/
