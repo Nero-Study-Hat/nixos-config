fe623ffa1d4af680168c4d8c9929b29d37216910
Nero-Study-Hat <nerostudyhat@gmail.com>
2024-08-17 17:58:25 -0400

    Add gitkeep and test call

--- modules/home/shell/default.nix
+++ modules/home/shell/default.nix
@@ -5,5 +5,11 @@
         ./bash.nix
         ./git.nix
         ./packages.nix
+        ./test.nix
     ];
+
+    home-modules.shell = {
+        defaults-enable = true;
+        extra-enable = true;
+    }
 }
